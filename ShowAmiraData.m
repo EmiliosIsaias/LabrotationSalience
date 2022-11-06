ac25bdda6db52e4d2bdd6125022a798c1d30dd78
Alexbmerkel <josephine.timm@gmx.de>
2022-11-06 13:45:13 +0100

    Update ShowAmiraData.m

--- ShowAmiraData.m
+++ ShowAmiraData.m
@@ -1,5 +1,4 @@
 cd ("Z:\Josephine\Histo\Cohort_12_33-38\#34\#34_reconstruction")
-addpath 'Z:\Josephine\MATLAB'
 clearvars
 
 % load Data
@@ -8,9 +7,11 @@ pom = LoadTiffStack('PO_stack.tif');
 vpm = LoadTiffStack('VPM_stack.tif');
 
 % plot Data
-figure
 [brain_faces, brain_vertices] = isosurface(brain);
-
+figure; p = patch('Faces', brain_faces, 'Vertices', brain_vertices);
+p.FaceColor = 'r';
+p.EdgeColor = 'none';
+p.FaceAlpha = 0.3;
 hold on
 isosurface(pom)
 isosurface(vpm)
